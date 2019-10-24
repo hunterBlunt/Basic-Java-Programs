4cd3b0acceb604e802cdf55fdb7934dc259b550c
hunterBlunt <53064417+hunterBlunt@users.noreply.github.com>
2019-10-24 04:35:39 -0500

    Add files via upload
    Needs a lot of work! trying to make it subtract battery as it does action and add battery when it charges.

---
+++ Droid.java
@@ -0,0 +1,42 @@
+package robot;
+
+public class Droid {
+	
+	String name;
+	String action;
+	String sum;
+	
+	public String toString() {
+		return name;
+		
+		
+	}
+	
+	//Constructor method
+		public void Droid() {
+			name = "Kyle";
+			action = "Kill all Humans!";
+			sum = name + 
+					action;
+		}
+	
+		
+		public void droidBat(double level, boolean isDoingAction) {
+			isDoingAction = true;
+			
+		
+			}
+					
+				
+		
+
+	public static void main(String[] args) {
+		
+		System.out.println());
+		
+		
+		
+		
+	}
+
+}
